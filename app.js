b55c4e54f7de18c089392cf70cc67d56533a9705
Florence Hapgood <flohapgood@icloud.com>
2018-11-13 11:23:59 +0000

    Debugging Heroku

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-var express = require("express"),
-app = express();
-app.get("theroutyouwant", function(req, res){
-// if you need to send a html file
-res.sendfile("index.html");
-//if you need to send html tag
-res.type("text/html");
-res.send("<p>hello world!</p>");
-};
-app.listen(3000);
-console.log('server is running on port 3000);
+var express = require('express');
+var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+// make express look in the public directory for assets (css/js/img)
+//app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
