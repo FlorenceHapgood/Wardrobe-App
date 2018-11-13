79f0b76ae6d7968ec08e514f1fb7b6db30474696
Florence Hapgood <flohapgood@icloud.com>
2018-11-13 11:34:55 +0000

    Debugging Heroku

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-var express = require('express');
-var app = express();
+var express = require("express");
+var app     = express();
+app.use(express.static(__dirname + '/views'));
+//Store all HTML files in view folder.
 
-//setting middleware
-app.use(express.static(__dirname + 'views')); //Serves resources from public folder
+app.get('/',function(req,res){
+  res.sendFile('index.html');
+  //It will find and locate index.html from View or Scripts
+});
 
 
-var server = app.listen(5000);
+app.listen(3000);
+
+console.log("Running at Port 3000");
