4ef75893415b9be8b4fc689704501f2fe346d763
Florence Hapgood <flohapgood@icloud.com>
2018-11-13 11:25:09 +0000

    Debugging Heroku

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 8080;
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 // make express look in the public directory for assets (css/js/img)
 //app.use(express.static(__dirname + '/public'));
 
